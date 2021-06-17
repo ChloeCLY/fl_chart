33e38dc5b1c2c5a96a1ae57c471ca0263d4bafbc
ansonfung2 <84693889+ansonfung2@users.noreply.github.com>
2021-06-17 12:52:51 +0800

    Update line_chart_painter.dart

--- lib/src/chart/line_chart/line_chart_painter.dart
+++ lib/src/chart/line_chart/line_chart_painter.dart
@@ -1275,12 +1275,7 @@ class LineChartPainter extends AxisChartPainter<LineChartData> {
         topLeft: radius, topRight: radius, bottomLeft: radius, bottomRight: radius);
 
     _bgTouchTooltipPaint.color = tooltipData.tooltipBgColor;
-    canvasWrapper.drawRRect(
-        roundedRect,
-        Paint()
-          ..color = Colors.black.withAlpha(shadowAlpha)
-          ..maskFilter =
-              MaskFilter.blur(BlurStyle.normal, 3 * 0.57735 + 0.5));
+    
     canvasWrapper.drawRRect(roundedRect, _bgTouchTooltipPaint);
 
     /// draw the texts one by one in below of each other
